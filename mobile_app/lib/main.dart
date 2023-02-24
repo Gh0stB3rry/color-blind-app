f4ee5dd35ad7da1ab3461fb655dc14ee016bfb33
ntm224 <ntm224@lehigh.edu>
2023-02-23 23:31:23 -0500

    Map places marker when finding your location

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -50,33 +52,40 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  Position _location = Position(latitude: 0.0, longitude: 0.0);
+  Position _location = Position(latitude: 39.8283, longitude: 98.5795);
+
+  late GoogleMapController mapController;
+  final LatLng _center = const LatLng(39.8283, 98.5795);
+
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
 
   void _displayCurrentLocation() async {
     final location = await Geolocator()
         .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    _add(location.latitude, location.longitude);
 
     setState(() {
       _location = location;
     });
   }
 
-  late GoogleMapController mapController;
-  final LatLng _center = const LatLng(45.521563, -122.677433);
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
+  void _add(lat, lng) {
+    var markerIdVal = "test";
+    final MarkerId markerId = MarkerId(markerIdVal);
+
+    final Marker marker = Marker(
+      markerId: markerId,
+      position: LatLng(lat, lng),
+      infoWindow: InfoWindow(title: markerIdVal, snippet: '*'),
+    );
+
+    setState(() {
+      markers[markerId] = marker;
+    });
   }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
   }
 
   @override
@@ -105,6 +114,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       target: _center,
                       zoom: 11.0,
                     ),
+                    markers: Set<Marker>.of(markers.values),
                   )),
             ],
           ),
