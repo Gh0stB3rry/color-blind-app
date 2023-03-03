414d750c47a044b982dad5842b065a063dc8deff
ntm224 <ntm224@lehigh.edu>
2023-03-03 13:17:03 -0500

    removed system icons from map

--- mobile_app/lib/maps.dart
+++ mobile_app/lib/maps.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
 import 'dart:math';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:mobile_app/comments.dart';
@@ -41,6 +44,24 @@ class _MyHomePageState extends State<MyHomePage> {
   String dropdownValue = collegeList.first;
 
   late GoogleMapController mapController;
+  //this is the function to load custom map style json
+  void changeMapMode(GoogleMapController mapController) {
+    getJsonFile("lib/assets/map_style.json")
+        .then((value) => setMapStyle(value, mapController));
+  }
+
+  //helper function
+  void setMapStyle(String mapStyle, GoogleMapController mapController) {
+    mapController.setMapStyle(mapStyle);
+  }
+
+  //helper function
+  Future<String> getJsonFile(String path) async {
+    ByteData byte = await rootBundle.load(path);
+    var list = byte.buffer.asUint8List(byte.offsetInBytes, byte.lengthInBytes);
+    return utf8.decode(list);
+  }
+
   final LatLng _center = const LatLng(40.6049, -75.3775);
 
   Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
@@ -84,6 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+    changeMapMode(mapController);
   }
 
   @override
