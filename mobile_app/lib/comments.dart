4dda09c7ea14eb95c701efb476dc63a8753952ed
ntm224 <ntm224@lehigh.edu>
2023-03-20 19:35:20 -0400

    Image uploading and sorting for comments

--- mobile_app/lib/comments.dart
+++ mobile_app/lib/comments.dart
@@ -1,16 +1,20 @@
 import 'dart:math';
 
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:mobile_app/home.dart';
 import 'package:mobile_app/main.dart';
 import 'package:mobile_app/maps.dart';
+import 'package:image_picker/image_picker.dart';
 
 const List<String> locationList = <String>[
   'Linderman Library',
   'Fairchild-Martindale Library',
-  'Lehigh Bookstore'
+  'Lehigh Bookstore',
+  'Images'
 ];
 
 class Comments extends StatelessWidget {
@@ -39,11 +43,67 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  File? galleryFile;
+  final picker = ImagePicker();
+
+  void _showPicker({
+    required BuildContext context,
+  }) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return SafeArea(
+          child: Wrap(
+            children: <Widget>[
+              ListTile(
+                leading: const Icon(Icons.photo_library),
+                title: const Text('Photo Library'),
+                onTap: () {
+                  getImage(ImageSource.gallery);
+                  Navigator.of(context).pop();
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.photo_camera),
+                title: const Text('Camera'),
+                onTap: () {
+                  getImage(ImageSource.camera);
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Future getImage(
+    ImageSource img,
+  ) async {
+    final pickedFile = await picker.pickImage(source: img);
+    XFile? xfilePick = pickedFile;
+    setState(
+      () {
+        if (xfilePick != null) {
+          setState(() {
+            _imgList.add(File(pickedFile!.path));
+          });
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(// is this context <<<
+              const SnackBar(content: Text('Nothing is selected')));
+        }
+      },
+    );
+  }
+
   TextEditingController cmntController = TextEditingController();
   String dropdownValue = locationList.first;
   String _lindermanList = "Comments: ";
   String _fmlList = "Comments: ";
   String _storeList = "Comments: ";
+  List<File> _imgList = [];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -51,8 +111,9 @@ class _MyHomePageState extends State<MyHomePage> {
         home: Scaffold(
             backgroundColor: Colors.lightGreen[100],
             body: Center(
-              child: Padding(
-                padding: const EdgeInsets.only(top: 30.0),
+                child: Padding(
+              padding: const EdgeInsets.only(top: 30.0),
+              child: SingleChildScrollView(
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: <Widget>[
@@ -128,6 +189,14 @@ class _MyHomePageState extends State<MyHomePage> {
                     ElevatedButton(
                       style: ElevatedButton.styleFrom(
                           backgroundColor: Colors.indigo.shade300),
+                      child: const Text('Select Image from Gallery and Camera'),
+                      onPressed: () {
+                        _showPicker(context: context);
+                      },
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.indigo.shade300),
                       onPressed: () {
                         //send comment to db
                         if (dropdownValue == "Linderman Library") {
@@ -169,15 +238,44 @@ class _MyHomePageState extends State<MyHomePage> {
                         } else if (dropdownValue ==
                             "Fairchild-Martindale Library") {
                           return _fmlList;
-                        } else {
+                        } else if (dropdownValue == "Lehigh Bookstore") {
                           return _storeList;
+                        } else {
+                          return "";
                         }
                       }()),
                       style: TextStyle(fontSize: 20),
                     ),
+                    SizedBox(
+                      height: 200.0,
+                      width: 300.0,
+                      child: _imgList.length < 1
+                          ? const Center(child: Text(''))
+                          : dropdownValue == "Images"
+                              ? Center(child: Image.file(_imgList[0]))
+                              : const Center(child: Text('')),
+                    ),
+                    SizedBox(
+                      height: 200.0,
+                      width: 300.0,
+                      child: _imgList.length < 2
+                          ? const Center(child: Text(''))
+                          : dropdownValue == "Images"
+                              ? Center(child: Image.file(_imgList[1]))
+                              : const Center(child: Text('')),
+                    ),
+                    SizedBox(
+                      height: 200.0,
+                      width: 300.0,
+                      child: _imgList.length < 3
+                          ? const Center(child: Text(''))
+                          : dropdownValue == "Images"
+                              ? Center(child: Image.file(_imgList[2]))
+                              : const Center(child: Text('')),
+                    ),
                   ],
                 ),
               ),
-            )));
+            ))));
   }
 }
