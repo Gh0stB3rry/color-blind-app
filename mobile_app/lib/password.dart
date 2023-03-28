65a80530ba7a76482f6ff45879e4b1f317fd2bbf
Kayla Kraft <kak524@Lehigh.edu>
2023-03-28 09:58:35 -0400

    password

---
+++ mobile_app/lib/password.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_app/home.dart';
+import 'package:mobile_app/main.dart';
+import 'package:mobile_app/maps.dart';
+import 'package:flutter/src/material/colors.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+
+class Signup extends StatelessWidget {
+  const Signup({Key? key}) : super(key: key);
+  static const String _title = 'Mental Health App';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.indigo,
+        ),
+      ),
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: const MyStatefulWidget(),
+        backgroundColor: Colors.lightGreen[100],
+      ),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView(
+          children: <Widget>[
+            Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child: DefaultTextStyle(
+                    style: TextStyle(fontSize: 30, color: Colors.black),
+                    child: AnimatedTextKit(
+                      animatedTexts: [
+                        TypewriterAnimatedText('Did you'),
+                        TypewriterAnimatedText('forget your password'),
+                      ],
+                    ))),
+            Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child: const Text(
+                  'Forgot Password',
+                  style: TextStyle(fontSize: 20),
+                )),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Forgot Password ',
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const MyApp()),
+                );
+              },
+              child: const Text(
+                'Back',
+              ),
+            ),
+            Container(
+                height: 50,
+                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                child: ElevatedButton(
+                  child: const Text('Submit'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Home()),
+                    );
+                  },
+                )),
+          ],
+        ));
+  }
+}
