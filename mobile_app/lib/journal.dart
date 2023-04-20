c8856784592efbcf3813014389028726a4be9035
ntm224 <ntm224@lehigh.edu>
2023-04-19 21:59:41 -0400

    Journal UI update

--- mobile_app/lib/journal.dart
+++ mobile_app/lib/journal.dart
@@ -131,189 +131,202 @@ class _MyHomePageState extends State<MyHomePage> {
         title: "Location",
         home: Scaffold(
             backgroundColor: Colors.lightGreen[100],
-            body: Center(
-                child: Padding(
-              padding: const EdgeInsets.only(top: 30.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Row(
+            body: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("lib/assets/mountain.jpg"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: Center(
+                    child: Padding(
+                  padding: const EdgeInsets.only(top: 30.0),
+                  child: Column(
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: <Widget>[
-                      SizedBox(width: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          SizedBox(width: 10),
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.lightGreen.shade300,
+                              minimumSize: Size(64, 64),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(50.0),
+                                  side: BorderSide(
+                                      color: Colors.lightGreen.shade300)),
+                            ),
+                            child: Icon(
+                              Icons.home,
+                              size: 30.0,
+                            ),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const Home()),
+                              );
+                            },
+                          ),
+                          SizedBox(width: 60),
+                          Text(
+                            "~ My Journal ~",
+                            style: GoogleFonts.lato(
+                                fontWeight: FontWeight.w700,
+                                fontSize: 20,
+                                color: Colors.indigo.shade300),
+                          )
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(
+                            "Date: " +
+                                now.month.toString() +
+                                "/" +
+                                now.day.toString() +
+                                "/" +
+                                now.year.toString(),
+                            style: GoogleFonts.lato(
+                                fontWeight: FontWeight.w500, fontSize: 16),
+                          ),
+                          Text(
+                              "Location: " +
+                                  location.latitude.toString() +
+                                  " " +
+                                  location.longitude.toString(),
+                              style: GoogleFonts.lato(
+                                  fontWeight: FontWeight.w500, fontSize: 16))
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      TextField(
+                        controller: cmntController,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Journal Entry',
+                        ),
+                      ),
                       ElevatedButton(
                         style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.lightGreen.shade300,
-                          minimumSize: Size(64, 64),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(50.0),
-                              side: BorderSide(
-                                  color: Colors.lightGreen.shade300)),
-                        ),
-                        child: Icon(
-                          Icons.home,
-                          size: 30.0,
-                        ),
+                            backgroundColor: Colors.indigo.shade300),
+                        child: const Text('Select Image'),
                         onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const Home()),
-                          );
+                          _showPicker(context: context);
                         },
                       ),
-                      SizedBox(width: 60),
-                      Text(
-                        "~ My Journal ~",
-                        style: GoogleFonts.lato(
-                            fontWeight: FontWeight.w700,
-                            fontSize: 20,
-                            color: Colors.indigo.shade300),
-                      )
-                    ],
-                  ),
-                  SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Text(
-                        "Date: " +
-                            now.month.toString() +
-                            "/" +
-                            now.day.toString() +
-                            "/" +
-                            now.year.toString(),
-                        style: GoogleFonts.lato(
-                            fontWeight: FontWeight.w500, fontSize: 16),
+                      SizedBox(
+                        height: galleryFile == null ? 0.0 : 200.0,
+                        width: galleryFile == null ? 0.0 : 300.0,
+                        child: galleryFile == null
+                            ? const Center(child: Text(''))
+                            : Center(child: Image.file(galleryFile!)),
                       ),
-                      Text(
-                          "Location: " +
-                              location.latitude.toString() +
-                              " " +
-                              location.longitude.toString(),
-                          style: GoogleFonts.lato(
-                              fontWeight: FontWeight.w500, fontSize: 16))
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.indigo.shade300),
+                        onPressed: () {
+                          setState(() {
+                            _journalList.add(cmntController.text);
+                            if (imgFlag) {
+                              _ImgList.add(galleryFile!);
+                              _imgBoolList.add(true);
+                              imgFlag = false;
+                            } else {
+                              _ImgList.add(null);
+                              _imgBoolList.add(false);
+                            }
+                            galleryFile = null;
+                            cmntController.clear();
+                          });
+                        },
+                        child: const Text('Add Entry'),
+                      ),
+                      Divider(color: Colors.black),
+                      Expanded(
+                          child: SizedBox(
+                        height: 200.0,
+                        child: ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: _journalList.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Container(
+                                      height: _imgBoolList[index] ? 150 : 50,
+                                      width: 250,
+                                      alignment: Alignment.center,
+                                      child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceEvenly,
+                                          children: [
+                                            Column(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.center,
+                                              children: [
+                                                Text(
+                                                    now.month.toString() +
+                                                        "/" +
+                                                        now.day.toString() +
+                                                        "/" +
+                                                        now.year.toString(),
+                                                    style: GoogleFonts.lato(
+                                                        fontWeight:
+                                                            FontWeight.w500,
+                                                        fontSize: 12)),
+                                                Text(
+                                                    "at " +
+                                                        location.latitude
+                                                            .toString() +
+                                                        " " +
+                                                        location.longitude
+                                                            .toString(),
+                                                    style: GoogleFonts.lato(
+                                                        fontWeight:
+                                                            FontWeight.w500,
+                                                        fontSize: 12)),
+                                              ],
+                                            ),
+                                            ElevatedButton(
+                                              style: ElevatedButton.styleFrom(
+                                                  backgroundColor:
+                                                      Colors.indigo.shade300),
+                                              child: Text('Show Message',
+                                                  style: GoogleFonts.lato(
+                                                      fontWeight:
+                                                          FontWeight.w500,
+                                                      fontSize: 12)),
+                                              onPressed: () {
+                                                showDialog(
+                                                  context: context,
+                                                  builder:
+                                                      (BuildContext context) =>
+                                                          _buildPopupDialog(
+                                                              context, index),
+                                                );
+                                              },
+                                            ),
+                                          ])),
+                                  _imgBoolList[index]
+                                      ? Container(
+                                          height: 150,
+                                          width: 130,
+                                          padding:
+                                              const EdgeInsets.only(bottom: 8),
+                                          child: Image?.file(_ImgList[index]!),
+                                        )
+                                      : SizedBox(width: 130)
+                                ]);
+                          },
+                        ),
+                      ))
                     ],
                   ),
-                  SizedBox(height: 10),
-                  TextField(
-                    controller: cmntController,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Journal Entry',
-                    ),
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.indigo.shade300),
-                    child: const Text('Select Image'),
-                    onPressed: () {
-                      _showPicker(context: context);
-                    },
-                  ),
-                  SizedBox(
-                    height: galleryFile == null ? 0.0 : 200.0,
-                    width: galleryFile == null ? 0.0 : 300.0,
-                    child: galleryFile == null
-                        ? const Center(child: Text(''))
-                        : Center(child: Image.file(galleryFile!)),
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.indigo.shade300),
-                    onPressed: () {
-                      setState(() {
-                        _journalList.add(cmntController.text);
-                        if (imgFlag) {
-                          _ImgList.add(galleryFile!);
-                          _imgBoolList.add(true);
-                          imgFlag = false;
-                        } else {
-                          _ImgList.add(null);
-                          _imgBoolList.add(false);
-                        }
-                        galleryFile = null;
-                        cmntController.clear();
-                      });
-                    },
-                    child: const Text('Add Entry'),
-                  ),
-                  Divider(color: Colors.black),
-                  Expanded(
-                      child: SizedBox(
-                    height: 200.0,
-                    child: ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: _journalList.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Container(
-                                  height: _imgBoolList[index] ? 150 : 50,
-                                  width: 250,
-                                  alignment: Alignment.center,
-                                  child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceEvenly,
-                                      children: [
-                                        Column(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          children: [
-                                            Text(
-                                                now.month.toString() +
-                                                    "/" +
-                                                    now.day.toString() +
-                                                    "/" +
-                                                    now.year.toString(),
-                                                style: GoogleFonts.lato(
-                                                    fontWeight: FontWeight.w500,
-                                                    fontSize: 12)),
-                                            Text(
-                                                "at " +
-                                                    location.latitude
-                                                        .toString() +
-                                                    " " +
-                                                    location.longitude
-                                                        .toString(),
-                                                style: GoogleFonts.lato(
-                                                    fontWeight: FontWeight.w500,
-                                                    fontSize: 12)),
-                                          ],
-                                        ),
-                                        ElevatedButton(
-                                          style: ElevatedButton.styleFrom(
-                                              backgroundColor:
-                                                  Colors.indigo.shade300),
-                                          child: Text('Show Message',
-                                              style: GoogleFonts.lato(
-                                                  fontWeight: FontWeight.w500,
-                                                  fontSize: 12)),
-                                          onPressed: () {
-                                            showDialog(
-                                              context: context,
-                                              builder: (BuildContext context) =>
-                                                  _buildPopupDialog(
-                                                      context, index),
-                                            );
-                                          },
-                                        ),
-                                      ])),
-                              _imgBoolList[index]
-                                  ? Container(
-                                      height: 150,
-                                      width: 130,
-                                      padding: const EdgeInsets.only(bottom: 8),
-                                      child: Image?.file(_ImgList[index]!),
-                                    )
-                                  : SizedBox(width: 130)
-                            ]);
-                      },
-                    ),
-                  ))
-                ],
-              ),
-            ))));
+                )))));
   }
 }
